2148e9cf598e0e77c202891964a5a8270e9a3776
Patrick Freinschlag <patrick.freinschlag@gmail.com>
2020-06-19 23:08:03 +0200

    ..

--- advanced/exercise 1/script.js
+++
@@ -1,7 +0,0 @@
-var numbersOne = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-var numbersTwo = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-for (var i = 0; i < numbersOne.length; i++) {
-    for (var j = 0; j < numbersTwo.length; j++) {
-        document.write(numbersTwo[j] + " x " + numbersOne[i] + " = " + numbersTwo[j] * numbersOne[i] + "<br>");
-    }
-}
